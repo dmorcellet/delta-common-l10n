a4ae3251291d43863a248acf53e52e1bc05ef7f5
Damien Morcellet <dmorcellet@free.fr>
2022-08-18 17:45:42 +0200

    Initial version based on code from lotro-core

---
+++ src/main/java/delta/common/utils/l10n/numbers/NumberFormatID.java
@@ -0,0 +1,25 @@
+package delta.common.utils.l10n.numbers;
+
+/**
+ * Identifiers for number formats.
+ * @author DAM
+ */
+public class NumberFormatID
+{
+  /**
+   * US format.
+   */
+  public static final String US="US";
+  /**
+   * French format.
+   */
+  public static final String FRENCH="FRENCH";
+  /**
+   * European format.
+   */
+  public static final String EUROPEAN="EUROPEAN";
+  /**
+   * Automatic: format chosen from the default locale.
+   */
+  public static final String AUTO="AUTO";
+}
